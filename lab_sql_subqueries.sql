1f0b27948b590a6957fbbcc4f3b6b54eec185716
Jules Albin <julesalbin@orange.fr>
2024-01-08 06:10:26 +0100

    Lab Solved

--- lab_sql_subqueries.sql
+++ lab_sql_subqueries.sql
@@ -1,19 +1,27 @@
 /*Write SQL queries to perform the following tasks using the Sakila database:
 -----------------------------------------------------------------------------*/
 
+use sakila;
+
 /* 1. Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
 ------------------------------------------------------------------------------------------------------------*/
 
-select count(*) as copies_of_Hunchback from (select title from film a inner join inventory b  on a.film_id=b.film_id where title='Hunchback Impossible') as c;
-
+select count(*) as copies_of_Hunchback 
+from (	select title 
+		from film 
+        join inventory using (film_id) 
+        where title='Hunchback Impossible') as flm;
 
 /* 2. List all films whose length is longer than the average length of all the films in the Sakila database.
 ------------------------------------------------------------------------------------------------------------*/
 
-
+select title from film where length > (select avg(length) from film ) ;
 
 /* 3. Use a subquery to display all actors who appear in the film "Alone Trip".
 -------------------------------------------------------------------------------*/
 
-
-
+select first_name, last_name 
+from actor 
+where actor_id in (	select actor_id 
+					from film_actor 
+                    where film_id = ( select film_id from film where title="Alone Trip"));
