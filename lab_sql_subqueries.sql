1772392e73264a82e13d63c28d5edcb1e250737f
Jules Albin <julesalbin@orange.fr>
2024-01-08 06:15:30 +0100

    Lab Solved

--- lab_sql_subqueries.sql
+++ lab_sql_subqueries.sql
@@ -7,10 +7,7 @@ use sakila;
 ------------------------------------------------------------------------------------------------------------*/
 
 select count(*) as copies_of_Hunchback 
-from (	select title 
-		from film 
-        join inventory using (film_id) 
-        where title='Hunchback Impossible') as flm;
+from (select title from film join inventory using (film_id) where title='Hunchback Impossible') as flm;
 
 /* 2. List all films whose length is longer than the average length of all the films in the Sakila database.
 ------------------------------------------------------------------------------------------------------------*/
@@ -20,8 +17,5 @@ select title from film where length > (select avg(length) from film ) ;
 /* 3. Use a subquery to display all actors who appear in the film "Alone Trip".
 -------------------------------------------------------------------------------*/
 
-select first_name, last_name 
-from actor 
-where actor_id in (	select actor_id 
-					from film_actor 
-                    where film_id = ( select film_id from film where title="Alone Trip"));
+select first_name, last_name from actor 
+where actor_id in (select actor_id from film_actor where film_id = ( select film_id from film where title="Alone Trip"));
